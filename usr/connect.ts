8f05111996773537aedf8f1c59f5b7e73d626735
Ademir <ademir@debian.local>
2023-02-23 20:06:58 -0300

    impl env

---
+++ usr/connect.ts
@@ -0,0 +1,8 @@
+const Client = require('pg').Client;
+require('dotenv').config()
+const user = process.env.DB_USER
+const password = process.env.DB_PASS
+const host = process.env.DB_HOST
+const port = process.env.DB_PORT
+const database = process.env.DB_BASE
+export const client = new Client({user, password, host, port, database})
--- usr/server.ts
+++ usr/server.ts
@@ -1,14 +1,12 @@
 import express, { NextFunction, Request, Response } from 'express';
-//const cors = require('cors')
+import { routerUser } from './routes/RouterUsers';
+
+const cors = require('cors')
 const app = express();
 const PORT = process.env.PORT || 3005
-//app.use(cors());
+app.use(cors());
 app.use(express.json());
-
-
-app.use('/', function (req, res) {
-    res.send('hello world')
-  })
+app.use(routerUser);
 
 app.use(
     (error: Error, request: Request, response: Response, next: NextFunction) => { 
