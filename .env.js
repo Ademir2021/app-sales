1df4e7eccb4e16723671ff8322aeca9edf73af5d
Ademir <ademir@debian.local>
2023-02-23 14:30:25 -0300

    ssl

--- .env.js
+++ .env.js
@@ -4,11 +4,9 @@ const config = {
         password: "1f2e7751b706515a7c5cf7a2f38674d0c945373fff0a27ed7d7ca607bc0e5a23",
         host: "ec2-3-217-113-25.compute-1.amazonaws.com",
         port: "5432",
-        database: "d7skldmpo5suds"
+        database: "d7skldmpo5suds",
+        sslmode:"require"
     },
-    ssl: {
-        rejectUnauthorized: false
-    }
 };
 
 module.exports = config;
